1032d193dddc0f8ec8ed21837b0a721cb479a592
Alexander Chernyi <mail@alexblack.pro>
2017-09-09 01:00:14 +0300

    Finish task 5 and task 6

--- lab1/lab1/main.cpp
+++ lab1/lab1/main.cpp
@@ -16,6 +16,12 @@ using namespace std;
 template<typename T>
 void showVectorItems(vector<T> vec, string message);
 
+template<typename T>
+void insertionSort(T* array, int size);
+
+template<typename T>
+void showArray(T* array, int size);
+
 // Bubble sort
 void task1() {
     vector<int> numbers = { 8, 9, 3, 1, 2, 4};
@@ -111,12 +117,52 @@ void task5() {
     } else if (vec.back() == 2) {
         for (auto it = vec.begin(); it != vec.end();) {
             if (*it % 3 == 0) {
-                
+                it = vec.insert(it + 1, 3, 1);
             } else {
                 it++;
             }
         }
     }
+    
+    showVectorItems(vec, "Result: ");
+}
+
+void fillRandom(double* array, int size) {
+    for (int i = 0; i < size; i++) {
+        array[i] = 1 - 2 * (double)rand() / (RAND_MAX);
+    }
+}
+
+void task6() {
+    int size = 5;
+    double array[5] = { };
+    fillRandom(array, size);
+    insertionSort(array, size);
+    showArray(array, size);
+    
+    size = 10;
+    double array2[10] = { };
+    fillRandom(array2, size);
+    insertionSort(array2, size);
+    showArray(array2, size);
+    
+    size = 25;
+    double array3[25] = { };
+    fillRandom(array3, size);
+    insertionSort(array3, size);
+    showArray(array3, size);
+    
+    size = 50;
+    double array4[50] = { };
+    fillRandom(array4, size);
+    insertionSort(array4, size);
+    showArray(array4, size);
+    
+    size = 100;
+    double array5[100] = { };
+    fillRandom(array5, size);
+    insertionSort(array5, size);
+    showArray(array5, size);
 }
 
 int main(int argc, const char * argv[]) {
@@ -125,6 +171,8 @@ int main(int argc, const char * argv[]) {
     task2();
     task3();
     task4();
+    //task5();
+    task6();
     
     return 0;
 }
@@ -133,5 +181,23 @@ template<typename T>
 void showVectorItems(vector<T> vec, string message) {
     cout << message;
     for (T item : vec) { cout << item << " "; }
+    cout << endl;
+}
+
+template<typename T>
+void insertionSort(T* array, int size) {
+    for (int i = 1; i < size; i++) {
+        int j = i;
+        
+        while ((j > 0) && (array[j] < array[j - 1])) {
+            swap(array[j], array[j - 1]);
+            j--;
+        }
+    }
+}
+
+template<typename T>
+void showArray(T* array, int size) {
+    for (int i = 0; i < size; i++) { cout << array[i] << " "; }
     cout << endl;
 }
