2558fcc8268c402e14bbd2d353a5b0c9974b2f89
Alexander Chernyi <mail@alexblack.pro>
2017-09-08 17:03:27 +0300

    lab1: Finish task2 and task3

--- lab1/lab1/main.cpp
+++ lab1/lab1/main.cpp
@@ -12,43 +12,75 @@
 using namespace std;
 
 template<typename T>
-void showVectorItems(vector<T> vec);
+void showVectorItems(vector<T> vec, string message);
 
 // Bubble sort
 void task1() {
     vector<int> numbers = { 8, 9, 3, 1, 2, 4};
     
-    showVectorItems(numbers);
+    showVectorItems(numbers, "Before bubble sort: ");
     
     for (int i = 1; i < numbers.size(); i++) {
         for (int j = 0; j < numbers.size() - i; j++) {
             
             if (numbers[j] > numbers[j + 1]) {
-                int temp = numbers[j];
-                numbers[j] = numbers[j + 1];
-                numbers[j + 1] = temp;
+                swap(numbers[j], numbers[j + 1]);
             }
         }
     }
     
-    showVectorItems(numbers);
+    showVectorItems(numbers, "After bubble sort: ");
 }
 
+
+// Insertion sort
 void task2() {
+    vector<int> numbers = { 8, 9, 3, 1, 2, 4};
     
+    showVectorItems(numbers, "Before insertion sort: ");
+    
+    for (int i = 1; i < numbers.size(); i++) {
+        int j = i;
+        
+        while ((j > 0) && (numbers[j] < numbers[j - 1])) {
+            swap(numbers.at(j), numbers.at(j - 1));
+            j = j - 1;
+        }
+    }
+    
+    showVectorItems(numbers, "After insertion sort: ");
+}
+
+//
+void task3() {
+    vector<int> numbers = { 8, 9, 3, 1, 2, 4};
+    
+    showVectorItems(numbers, "Before insertion sort: ");
+    
+    for (int i = 1; i < numbers.size(); i++) {
+        int j = i;
+        
+        while ((j > 0) && (numbers[j] < numbers[j - 1])) {
+            swap(numbers.at(j), numbers.at(j - 1));
+            j = j - 1;
+        }
+    }
+    
+    showVectorItems(numbers, "After insertion sort: ");
 }
 
 int main(int argc, const char * argv[]) {
     
     task1();
     task2();
+    task3();
     
     return 0;
 }
 
 template<typename T>
-void showVectorItems(vector<T> vec) {
-    cout << "Vector items: ";
+void showVectorItems(vector<T> vec, string message) {
+    cout << message;
     for (T item : vec) { cout << item << " "; }
     cout << endl;
 }
